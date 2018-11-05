acb585a0892b039d846ad38d4041d34561fdd444
Hugo <hugo.chapelain1@gmail.com>
2018-11-05 18:00:44 +0100

    :sparles: Add shoot targets

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 const shooter = {}
 
-// 
-// Set Up
-// 
-
+/**
+ * Set up
+ */
 shooter.$container = document.querySelector('.shooter')
 shooter.$start = shooter.$container.querySelector('.start')
-shooter.$score = shooter.$container.querySelector('.score')
+shooter.$score = shooter.$container.querySelector('.score .value')
 shooter.$timer = shooter.$container.querySelector('.timer')
 shooter.$targets = shooter.$container.querySelector('.targets')
 
-shooter.addTarget = () => {
+shooter.score = 0
+
+/**
+ * Methods
+ */
+shooter.addTarget = () =>
+{
+    // Create target
     const $target = document.createElement('div')
     $target.classList.add('target')
     $target.style.top = `${Math.random() * 100}%`
     $target.style.left = `${Math.random() * 100}%`
     shooter.$targets.appendChild($target)
+
+    // Listen to mouse enter
+    $target.addEventListener('mouseenter', () =>
+    {
+        shooter.shootTarget($target)
+    })
+}
+
+shooter.shootTarget = (_$target) =>
+{
+    // Delete target
+    _$target.remove()
+
+    // Add new target
+    shooter.addTarget()
+
+    // Increment score
+    shooter.score++
+    shooter.$score.textContent = shooter.score
+    // Play sound
 }
 
 shooter.addTarget()
